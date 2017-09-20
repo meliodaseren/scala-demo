b943fdb67486190cd8e8f5e33cb1a05cd15827e9
meliodaseren <julian07027@gmail.com>
2017-09-20 19:18:54 +0800

    Add Case Class

---
+++ src/ScalaProgramming/ch3/caseClass.scala
@@ -0,0 +1,26 @@
+package ScalaProgramming.ch3
+
+/* Case Class 不常定義成員函數 */
+case class Teacher(id: String, name: String, age: Int)
+
+case class SomeCaseClass(x: Int, y: Int)
+
+object caseClass {
+
+  def main(args: Array[String]): Unit = {
+
+    val eren = Teacher("0000", "Eren", 25)
+
+    // 上式等同於：
+    val eren2 = Teacher.apply("0000", "Eren", 25)
+
+    val a = SomeCaseClass(1, 2)
+    val b = SomeCaseClass(1, 2)
+
+    println(a == b)
+  }
+}
+
+
+
+
